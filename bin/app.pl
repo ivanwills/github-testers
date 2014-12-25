ca3b20b7c87ad148a40e1216cc5c830c02c21914
Ivan Wills <ivan.wills@gmail.com>
2014-12-25 18:26:27 +1100

    Initial commit

---
+++ bin/app.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/env perl
+use Dancer;
+use Github::Testers;
+dance;
---
+++ lib/Github/Testers.pm
@@ -0,0 +1,10 @@
+package Github::Testers;
+use Dancer ':syntax';
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+true;
